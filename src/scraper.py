3aa6d069c1d7585103eb273f4a01dd9859b45e48
Henrik Christensen <sensen1695@hotmail.com>
2018-01-06 20:35:18 +0100

    Scraper refactored

--- src/scraper.py
+++ src/scraper.py
@@ -3,12 +3,9 @@ import requests
 
 
 class Scraper:
-    def make_request(self, url):
-        r = requests.get(url)
-        return r.text
-
     def scrape_url(self, url):
-        soup = BeautifulSoup(self.make_request(url), 'html.parser')
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
         title_arr = soup.title.string.split()
         scraped_title = ' '.join(title_arr[:-2])  # slicing off '- Youtube' at the end of the title
         return scraped_title
